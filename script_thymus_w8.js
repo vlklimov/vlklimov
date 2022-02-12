5c1d9d1de026d70b887ce04ae4d2b97fe2f15583
vlklimov <99549537+vlklimov@users.noreply.github.com>
2022-02-12 22:55:17 +0700

    Add files via upload

---
+++ script_thymus_w8.js
@@ -0,0 +1,37 @@
+синтаксис проверен, но не работает
+
+function compute() {
+	var weight = 0;
+	var t_age = 0;
+	if(t_age == one) {
+		var t_weight = "4.2*weight";
+		document.THYMUS.thymus.value = t_weight; {
+			if(t_age == two) {
+				t_weight = "2.2*weight";
+				document.THYMUS.thymus.value = t_weight; {
+					if(t_age == three) {
+						t_weight = "1.2*weight";
+						document.THYMUS.thymus.value = t_weight; {
+							if(t_age == four) {
+								t_weight = "0.9*weight";
+								document.THYMUS.thymus.value = t_weight; {
+									if(t_age == five) {
+										t_weight = "0.6*weight";
+										document.THYMUS.thymus.value = t_weight; {
+											if(t_age == six) {
+												t_weight = "0.4*weight";
+												document.THYMUS.thymus.value = t_weight; {
+													if(t_age == seven) {
+														t_weight = "0.3*weight";
+														document.THYMUS.thymus.value = t_weight; {
+															if(t_age == eight) {
+																t_weight = "0.2*weight";
+																document.THYMUS.thymus.value = t_weight; {
+																	if(t_age == nine) {
+																		t_weight = "0.1*weight";
+																		document.THYMUS.thymus.value = t_weight; {
+																			if(t_age == ten) t_weight = "0.05*weight";
+																			document.THYMUS.thymus.value = t_weight; {
+																				if(t_age === 0) {
+																					t_weight = false;
+																				}
